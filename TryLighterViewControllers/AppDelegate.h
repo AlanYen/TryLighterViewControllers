1a4d318d3be9867087391c4fb7311dbbabd239fb
AlanYen <alanyen.mobile@gmail.com>
2016-03-08 12:48:19 +0800

    Initial Commit

---
+++ TryLighterViewControllers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TryLighterViewControllers
+//
+//  Created by AlanYen on 2016/3/8.
+//  Copyright © 2016年 17Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TryLighterViewControllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TryLighterViewControllers
+//
+//  Created by AlanYen on 2016/3/8.
+//  Copyright © 2016年 17Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
